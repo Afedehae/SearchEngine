66ad84ab3d742c6e46a85109be1747bb07cd3ad3
Afedehae <103344275+Afedehae@users.noreply.github.com>
2022-05-23 21:16:38 -0300

    Add files via upload

---
+++ SearchEngine/Files/SearchEngine.js
@@ -0,0 +1,123 @@
+function Hyde(){
+  var a = document.getElementById('q').value;
+  if(a === '123234345456567678789890'){
+    document.getElementById('xxx').hidden = (false);
+  } else {
+    alert("Não toque a criança");
+  }
+}
+function DuckGo(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://duckduckgo.com/?t=ffab&q=' + a + '&atb=v315-1&ia=web');
+}
+function Google(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://www.google.com/search?q=' + a);
+}
+function Yandex(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://yandex.com/search/?text=' + a +'&lr=10136&lang=en');
+}
+function Yahoo(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://br.search.yahoo.com/search?p=' + a + '&fr=yfp-t&fr2=p%3Afp%2Cm%3Asb&ei=UTF-8&fp=1')
+}
+function Brave(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://search.brave.com/search?q=' + a + '&source=web');
+}
+function GitHub(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://github.com/search?q=' + a);
+}
+function GoogleScholar(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://scholar.google.com/scholar?hl=pt-PT&as_sdt=0%2C5&q=' + a + '&btnG=&oq=')
+}
+function Youtube(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://www.youtube.com/results?search_query=' + a);
+}
+function Odysee(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://odysee.com/$/search?q=' + a);
+}
+function WebCrawler(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://www.webcrawler.com/serp?q=' + a + '&sc=qt4B80kUecx7Q0eCdeXhTIiO7GpTWjL9w4KMUWDPQniX6JGJgKjd32RvzaLRNGA1oAVCA20aoOoUkRi6SAK952TeBO61ZwdA1eVtRSl6Pt85j4hEcffp2TKu4t05-ow2Am7eHrWJv3MV6dfBE77QUt8XieaS-R7SMDpkD9M6JzKphXknMR8e4hU1C_hH98iIaJsa3qTRIdZUYHgnMkWEELhosXv4D8NBXGVNsifhMBaDC9f2uAZWc845Ia9AAwGE5l2Go0YG15HSJnAajGbqi3v9I4quqQAuOHTKxa-yOZfsvRPhCbvQ2aktH5eDUxkLsr1jBjFuwUrtMKkhpsXeLdk0DWkEeXODpTyQnd3DM8OHupAXiajIfknnIR4bt8ThcUH1QVt2rpMtP-lAH10yT5FfH4Q807iyyBxXNvTgs3EVh-kEu67EJ-UwpEM8qAqwZWfC1knykSUFwmvF01OdkdlTghIfPBq-LtzfUXeiyCES1SaQR2jiwyqBRS8mCLWLjwrCJNSVN1PjoIlfFQNhlk5sajRxe_f36ARu4pUoALGBkfp1gY6KK1pD2qV1GYopfNTXudjDzR0FO24NMnK8N08jEp4xYuGoE0a71IaHrm2NlH-MkrpyE_ssYFxxNual5NT4V5TbOWKMjZxLaNH_h5HqsUdqAvmDF_E6_zL5PwYWk4c2ssTZvQ7EGpNi1gRpMzMlu4Dq4ZGNGElTVjNTrTFRTqG8WHQyluvT1qVqIn-yAO2VdiycHhapPQtm8VlnblHQGeXl2R3YL6ft10eSt-UnXcntz_md_xtToWA7cXh_ofDrhz-ttSsRT_IzkqSfpwLD5xvaP0pqo-m_OUDqb7M38DPc5ARrPDwMIUDxCwOzBtIMNkrNhC1DhL82nX4zAqm-bYu6fQBQ72tpyeC-gtuMfsqhlECXXWi3sNOsklIPmndE0J3wS0YSB-VzOy41PiKpQlaLWQJb39ygbeFogpG6RqKTt9YulEmVRBYSz3XuYGUvkzWFg9LL6iLs4qLJidvoGY4Gvqx7CBN2P12BQLC1AnYtws1j50kweHLcXg741EpGc9gxHAtk5xHmgodXvGGrbBERXM28DHmMqUEn6yyqmzUd9vfx6xj92yCaJnSFduY36fwcVpnr2F221BO9Wn8ZfDwaBTnUseQbJKtL9jhzyXSb37EEWqrnnNr2fMEAKaoj6_zp6iZ7azXOdSmxarCm3yRrxt1Zo3-D5M03iMGc9BhXchWH-eJ16_X8_x4vsT1gErrXRERA6mbyw-QvyDa0OW7sXuT07k8BbM-BU4O_G6N04B3fs7FhcHsgR14Vv721tcTGQyFoQHhdU8PoCvvSCEdMLSQSL2FUgtTVrsxyP3PCLH8x67tukxtlxLhI109GBhc91WflUchGB-rudClut325qUI1hu8UZV2mkusBfO-yy0Yr9H3W9bpfrzODMJNxDv1HBto-8LZIgXDcty-iPfkEG6VSAqITr44hWGQLMylOXjbIDQ17K5O87yNH5cmhLLaMiL6ExDlzunGDvUOuNJChfa7PBPUOCNSK3ssDBaFitSh7R4b0qM3eryXXzsEeZmZSNENBuBBwSws2LM18_f2u04NKIPo1Z43mH6v6L6doiN8tR2uCUcQNzYBF75F236YEMNj0bhWwmAgY_ZLXb55ftivwQgvNo_DhR938gGKfxYlGX_hDKuE95lwB18cvDG2ptmILikcxgvRrBqCgWd-olBDFpfCL8bMNTJe7rFUVKYIh6Xg9BC_wKOVvRUsatFMhYotTdT3toGupYTHp_GiOADBwgkpoKMhJDOcdpyMm44R-pIBER0QwEEDPT2Ns5btFK-4qdpYb0crKS4r18E7uWXAD_eNdvmXy-voXehLNcFTDcGNEqvuHvE-5Ljw');
+}
+function Pdfdrive(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://www.pdfdrive.com/search?q=' + a + '&pagecount=&pubyear=&searchin=pt&em=');
+}
+function ThePirateBay(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://thepiratebay.org/search.php?q=' + a + '&all=on&search=Pirate+Search&page=0&orderby=');
+}
+function LibGen(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://libgen.is/search.php?req=' + a + '&lg_topic=libgen&open=0&view=simple&res=25&phrase=1&column=def');
+}
+function WikiBooks(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://en.wikibooks.org/w/index.php?search=' + a +'&title=Special%3ASearch&go=Go&ns0=1&ns4=1&ns102=1&ns110=1&ns112=1');
+}
+function CrackedGames(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://cracked-games.org/?s=' + a);
+}
+function SkidrowGames(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://www.skidrow-games.com/?s=' + a + '&x=50&y=19');
+}
+function PornHub(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://pt.pornhub.com/video/search?search=' + a);
+}
+function Xvideos(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://www.xvideos.com/?k=' + a);
+}
+function DarkPorn(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://darknessporn.com/?s=' + a);
+}
+function MundoHentai(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '+');
+  window.open('https://mundohentaioficial.com/?s=' + a);
+}
+function Rule34(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '_');
+  window.open('https://rule34.xxx/index.php?page=post&s=list&tags=' + a + '+');
+}
+function BestGore(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '_');
+  window.open('https://bestgore.fun/search?search=' + a + '&searchTarget=local')
+}
+function RapeLust(){
+  var a = document.getElementById('q').value;
+  a = a.replace(/ /gi, '_');
+  window.open('https://rapelust.com/search/' + a);
+}
---
+++ SearchEngine/Files/ha.js
@@ -0,0 +1,86 @@
+var chance = 5;
+var limite = 0;
+var cancer= [
+  'https://www.youtube.com/watch?v=rlkSMp7iz6c',
+  'https://www.youtube.com/watch?v=TL470fJMi7w&t=22s',
+  'https://www.youtube.com/watch?v=Zc09khxyXfA',
+  'https://www.xvideos.com/video67136933/chupando_meu_tio_enquanto_a_esposa_dele_esta_assistindo_tv',
+  'https://www.xvideos.com/video69128901/ensinei_o_bluezao_youtuber_a_dar_prazer_pra_atriz_duda_hugnen_-_me_sigam_em_minhas_redes_sociais_instagram_e_twitter_alicehaelxv',
+  'https://www.xvideos.com/video65789879/convidei_o_uber_pra_subir_e_ele_me_socou_forte_completo_no_xvideosred_',
+  'https://www.xvideos.com/video54606483/alice_hael_dando_uns_pegas_gostosos_com_a_duda_hugnen',
+  'https://www.xvideos.com/video66942619/mamando_o_bluezao_ate_ele_gozar',
+  'https://www.xvideos.com/video69821577/fiz_um_beijo_grego_no_bluezao_youtuber_-_me_sigam_no_insta_e_no_twitter_alicehaelxv',
+  'https://www.xvideos.com/video67908225/gozei_e_soltei_bastante_leitinho_pra_voces_instagram_alicehaelxv',
+  'https://www.xvideos.com/video67136325/chupando_o_casado_enquanto_a_esposa_dele_assiste',
+  'https://www.xvideos.com/video69129607/tigresavip_aceitou_minha_proposta_de_comer_o_bluezao_com_vibrador_me_siga_em_minhas_outras_redes_sociais_alicehaelxv',
+  'https://www.xvideos.com/video66923361/mamando_ate_engolir_todo_o_leitinho_do_bluezao',
+  'https://www.xvideos.com/video66807051/hipnotizador_me_fazendo_ter_um_orgasmo_hipnotico_publicamente_sem_sexo',
+  'https://www.xvideos.com/video65740033/novinho_dotado_da_favela_me_comeu_gostoso_video_completo_no_xvideosred_',
+  'https://www.xvideos.com/video69748697/trans_operada_chupando_e_dando_gostoso_-_me_sigam_no_insta_e_twitter_alicehaelxv',
+  'https://www.xvideos.com/video68983887/ninfetinha_kawaii_se_tocando_ao_som_de_pode_leitar_-_segue_la_no_instagram_ou_twitter_alicehaelxv',
+  'https://www.xvideos.com/video66705487/pagaram_100_reais_para_o_bluezao_beber_a_propria_porra_e_eu_ajudei_ele_com_isso',
+  'https://www.xvideos.com/video69738135/batendo_uma_cheia_de_tesao_pra_aliviar_apos_ser_assaltada_-_me_sigam_no_instagram_e_twitter_alicehaelxv',
+  'https://www.xvideos.com/video69748625/compilacao_os_melhores_videos_anal_que_ja_fiz_-_me_siga_no_instagram_e_no_twitter_alicehaelxv',
+  'https://www.xvideos.com/video69754879/brincando_com_a_piroca_do_ursinho_pirocudo_-_feat_duda_hugnen_-_me_sigam_no_twitter_e_instagram_alicehaelxv',
+  'https://www.xvideos.com/video55850217/alice_hael_brincando_com_seus_peitos_anunciando_sua_volta_para_o_xvideos',
+  'https://www.xvideos.com/video69749383/comparando_quem_mama_mais_gostoso_eu_ou_a_tigresavip_-_me_segue_la_no_instagram_e_twitter_alicehaelxv',
+  'https://www.xvideos.com/video69757569/me_tocando_bem_gostoso_e_mostrando_como_meu_corpo_mudou_-_me_sigam_no_instagram_e_twitter_alicehaelxv',
+  'https://www.xvideos.com/video69748493/compilacao_das_minhas_melhores_mamadas_chupando_pau_e_buceta_-_feat_duda_hugnen_-_segue_no_insta_e_twitter_alicehaelxv',
+  'https://www.xvideos.com/video52269211/duda_hugnen_dando_uns_amassos_na_alice_hael_no_video_de_verificacao_para_o_xvideos',
+  'https://www.youtube.com/watch?v=4YAkFs3R9GY',
+  'https://www.youtube.com/watch?v=xNknOFRfUnA&t=55s',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F0c%2Fb1%2Fee%2F0cb1eeab893355f3de939866ad8feb4d.jpg&f=1&nofb=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6eBjYhcrX5RqVIJF5u4bggHaMc%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.e20Gg5u-zseojj-JfqsGDgHaD4%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.Y75aR8qHoKjTOVHfKMcDkwHaHU%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.jsXikjK2Adsn9qgqIh_KxQHaEK%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.f_T5qquNuUcHvvRA29QCnwHaJl%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.mOy8vzJuoexnLe6C5HcNAwHaHe%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.Ma2VNTTLSdFaH-2O6H_bTAHaEK%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.6BKbPg5FquuAchIbHY1d-QHaJ3%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.d8nL5lJ4dG-Cnq-uupw3rAHaEK%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.lGPY40ZtgsebsB4naw9b8QHaEK%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.explicit.bing.net%2Fth%3Fid%3DOIP.SNzdh-zkVmZ2dWFnUjv-nwHaEK%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.OFoPTT8E-bOPmNixWAnp2AHaG-%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.YQxooHCQLmjTVXflPw7rggHaNH%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.4Ocp0fi29d4iUWJB6N62sAHaE-%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Wi1x4-EkZ7LI6VkuOpWPvwHaHl%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.GpBysU1MHRPSKaTnz336PgHaFj%26pid%3DApi&f=1',
+  'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F0c%2Fb1%2Fee%2F0cb1eeab893355f3de939866ad8feb4d.jpg&f=1&nofb=1',
+  'https://rule34.xxx/index.php?page=post&s=view&id=6083542',
+  'https://rule34.xxx/index.php?page=post&s=view&id=6102412',
+  'https://rule34.xxx/index.php?page=post&s=view&id=5302503',
+  'https://www.youtube.com/watch?v=ettaeKZHAwA&t=44s',
+  'https://www.youtube.com/watch?v=gf_-9fICq8g',
+  'https://www.youtube.com/watch?v=GgDTl3isb1Q',
+  'https://www.youtube.com/watch?v=5IsSpAOD6K8'
+]
+function Hyde(){
+  var a = document.getElementById('q').value;
+  if(a === '123234345456567678789890'){
+    document.getElementById('xxx').hidden = (false);
+  } else if (chance == 5) {
+    alert('Não toque na criança')
+    --chance;
+  } else if (chance == 4) {
+    alert('Estou te falando para não mexer na criança')
+    --chance;
+  } else if (chance == 3) {
+    alert('Você está me deixando sem escolhas...')
+    --chance;
+  } else if (chance == 2) {
+    alert('Se você mexer nela de novo, você vai ver');
+    --chance;
+  } else if (chance == 1) {
+    alert('Este é o seu ultimo aviso...');
+    --chance;
+  } else {
+    alert('Eu te disse para não tocar na criança');
+    setTimeout(pain(), 1);
+  }
+}
+function pain(){
+  var caos = Math.floor(Math.random()*cancer.length);
+  window.open(cancer[caos]);
+  setTimeout(pain(), 1);
+}
